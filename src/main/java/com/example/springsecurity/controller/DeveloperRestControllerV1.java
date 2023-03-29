c0ef8246154cecb4c048526290fd1cbd10f72d82
Ant0n2107 <antontertyshnik@mail.ru>
2023-03-29 17:22:43 +0300

    PreAuthorize annotation

--- src/main/java/com/example/springsecurity/controller/DeveloperRestControllerV1.java
+++ src/main/java/com/example/springsecurity/controller/DeveloperRestControllerV1.java
@@ -1,6 +1,7 @@
 package com.example.springsecurity.controller;
 
 import com.example.springsecurity.model.Developer;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,6 +21,7 @@ public class DeveloperRestControllerV1 {
         return DEVELOPERS;
     }
     @GetMapping("/{id}")
+    @PreAuthorize("hasAuthority('developers:read')")
     public Developer getById(@PathVariable Long id){
         return DEVELOPERS.stream().filter(
                 dev -> dev.getId().equals(id))
@@ -27,11 +29,13 @@ public class DeveloperRestControllerV1 {
                 .orElse(null);
     }
     @PostMapping
+    @PreAuthorize("hasAuthority('developers:write')")
     public Developer create(@RequestBody Developer developer){
         this.DEVELOPERS.add(developer);
         return developer;
     }
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasAuthority('developers:write')")
     public void deleteById(@PathVariable Long id){
         this.DEVELOPERS.removeIf(dev -> dev.getId().equals(id));
     }
